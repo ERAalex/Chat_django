54a11d9da78502a73efdc4d0a39c14e36b1b5bc2
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-04-27 11:11:03 +0300

    done login form and template

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
 
 def frontpage(request):
     return render(request, 'chatapp/frontpage.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect('frontpage')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'chatapp/signup.html', {'form': form})
