713ceeba2b9308081ecbc200dc77c8dca11be574
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-04-27 11:18:06 +0300

    done sign up part and form + models

--- chatapp/forms.py
+++ chatapp/forms.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2']
+        fields = ['username', 'password1', 'password2']
 
